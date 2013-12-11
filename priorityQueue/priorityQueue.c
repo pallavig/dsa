47f0a02760869b50cf944d7f830a1c9b847515d7
Pallavi <pallavig@thoughtworks.com>
2013-12-11 18:56:43 +0530

    priority queue added

---
+++ priorityQueue/priorityQueue.c
@@ -0,0 +1,57 @@
+#include "priorityQueue.h"
+#include <string.h>
+#include <stdlib.h>
+pQueue* create(){
+	pQueue* pqueue = calloc(1,sizeof(pQueue));
+	return pqueue;
+};
+
+Node* createNode(char* name,int priority,int time){
+	Node* node = calloc(1,sizeof(Node));
+	node->attempts = 0;
+	strcpy(node->name,name);
+	node->priority = priority;
+	node->time = time;
+	return node;
+}
+
+void insertAtBeginning(Node* nodeToInsert,Node* node,pQueue* pqueue){
+	nodeToInsert->next = node;
+	pqueue->head = nodeToInsert;
+}
+
+void insertAfterSpecifiedNode(Node* nodeToInsert,Node* node,pQueue* pqueue){
+	nodeToInsert->next = node->next;
+	node->next = nodeToInsert;	
+}
+
+void insert(pQueue* pqueue,char* name,int priority,int time){
+	Node* node,*pv;
+	Node* nodeToInsert = createNode(name,priority,time);
+	if(NULL == pqueue->head){
+		pqueue->head = nodeToInsert; pqueue->length++;
+		return;
+	}
+	node = pqueue->head;
+	while(node!=NULL){
+		if(node->priority < nodeToInsert->priority)
+			break;
+		pv = node;
+		node = node->next;
+	}
+	if(node == NULL){
+		node = pv;
+		insertAtBeginning(nodeToInsert,node,pqueue);
+		return;
+	}
+	insertAfterSpecifiedNode(nodeToInsert,node,pqueue);
+	return;
+}
+
+Node* remove(pQueue *pqueue){
+	Node* node;
+	node = pqueue->head;
+	pqueue->head = pqueue->head->next;
+	node->next = NULL;
+	return node;
+}
---
+++ priorityQueue/priorityQueue.h
@@ -0,0 +1,15 @@
+typedef struct node {
+	char name[10];
+	struct node* next;
+	int priority;
+	int time;
+	int attempts;
+} Node;
+typedef struct {
+	Node* head;
+	int length;
+} pQueue;
+
+pQueue* create();
+void insert(pQueue* pqueue,char* name,int priority,int time);
+Node* remove(pQueue* pqueue);
