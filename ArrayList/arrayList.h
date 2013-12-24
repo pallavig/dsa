0fead05901e2f69888d65b10ec1c3e9d265d2d5a
Pallavi <pallavig@thoughtworks.com>
2013-12-24 12:39:25 +0530

    remove and getLength done

---
+++ ArrayList/arrayList.h
@@ -0,0 +1,15 @@
+typedef struct {
+	void** base;
+	int capacity;
+	int length;
+} ArrayList;
+
+ArrayList create(int capacity);
+
+int insert(ArrayList *list, int index, void* data);
+int remove(ArrayList *list,int index);
+
+int getLength(ArrayList *list);
+void* get(ArrayList *list, int index);
+
+void dispose(ArrayList *list);
